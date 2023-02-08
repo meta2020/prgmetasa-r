e1267965ef08c4bf275b5adcdcbbaebd15222ebe
Yi <zhouyi0426@hotmail.com>
2023-02-08 21:06:43 +0800

    update

---
+++ simulation-results/U14/1000-times-simulation.R
@@ -0,0 +1,104 @@
+##******************************************************************************
+##
+## SIMULATION TO COMPARE COVERAGE % OF TNM IN POPULATION
+##
+##******************************************************************************
+
+{
+  
+  rm(list = ls())
+  
+  library(foreach)
+  library(parallel)
+  library(doSNOW)
+  library(doRNG)
+  library(survival)
+  library(testit)
+  ## LOAD FUNCTIONS
+  
+  files.sources <- list.files(path = "../../R/")
+  x <- sapply(paste0("../../R/", files.sources), source)
+  rep <- 1000
+  
+}
+
+tK0 <- 2
+
+dist1 <- c(N.min = 50, N.max = 150)
+dist2 <- c(N.min = 50, N.max = 300)
+pts.dist <- dist1
+
+b0 <- 1
+
+
+
+ncores <- 120
+cl <- makeCluster(ncores)
+doSNOW::registerDoSNOW(cl)
+set.seed(2023)
+
+
+##******************************************************************************
+##
+## DISTRIBUTION OF PATIENTS: 1 ----
+##
+##******************************************************************************
+
+pts.dist <- dist1
+ptdis <- "ptdis1"
+load(sprintf("a-calc/beta%s_pts%s_%s.RData", b0, pts.dist[1], pts.dist[2]))
+
+for (N in c(20,30,50,100)){
+  
+  ## BIOMARKER 1----
+  alpha <- rev(set$alpha1)
+  mk <- set$mk1
+  hr <- "HR1"
+  source("loop-hr-incorrect.R")
+  
+  ## BIOMARKER 2----
+  alpha <- rev(set$alpha2)
+  mk <- set$mk2
+  hr <- "HR2"
+  source("loop-hr-incorrect.R")
+
+  
+}
+
+
+##******************************************************************************
+##
+## DISTRIBUTION OF PATIENTS: 2 ----
+##
+##******************************************************************************
+
+pts.dist <- dist2
+ptdis <- "ptdis2"
+load(sprintf("a-calc/beta%s_pts%s_%s.RData", b0, pts.dist[1], pts.dist[2]))
+
+
+for (N in c(20,30,50,100)){
+  
+  ## BIOMARKER 1----
+  alpha <- rev(set$alpha1)
+  mk <- set$mk1
+  hr <- "HR1"
+  source("loop-hr-incorrect.R")
+  
+  ## BIOMARKER 2----
+  alpha <- rev(set$alpha2)
+  mk <- set$mk2
+  hr <- "HR2"
+  source("loop-hr-incorrect.R")
+  
+  
+}
+
+
+parallel::stopCluster(cl)
+
+
+
+
+
+
---
+++ simulation-results/U14/loop-hr-incorrect.R
@@ -0,0 +1,14 @@
+
+for (p in seq(0.9, 0.1, -0.1)){
+S0 <- round(N/p)
+DATA <- foreach(r = 1:rep, .combine = "cbind", .packages = c("survival"), .errorhandling = "remove")  %dorng%  {  # 
+  
+  simu(S = S0, tK = tK0, b = b0, a = alpha[p*10], N.min = pts.dist[1], N.max= pts.dist[2], 
+       CD = "UNIF", minc=1, maxc=4, 
+       x1.u = mk[1], x2.u = mk[2], x1.s = mk[3], x2.s = mk[4], v.sd = mk[5],
+       true.p = NULL)
+  
+}
+save(DATA, file = sprintf("N%d/%s_%s_p%.1f.RData", N, ptdis, hr, p))
+
+}
